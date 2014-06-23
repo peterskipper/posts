e94da81a81964e9af8ab37f219f8bbf54cf13431
Joe Turner <joe@oampo.co.uk>
2014-06-23 13:02:05 +0100

    Initial commit

---
+++ posts/config.py
@@ -0,0 +1,7 @@
+class DevelopmentConfig(object):
+    DATABASE_URI = "sqlite:///posts-development.db"
+    DEBUG = True
+
+class TestingConfig(object):
+    DATABASE_URI = "sqlite://"
+    DEBUG = True
---
+++ tests/api_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import os
+import json
+from urlparse import urlparse
+
+# Configure our app to use the testing databse
+os.environ["CONFIG_PATH"] = "posts.config.TestingConfig"
+
+from posts import app
+from posts import models
+from posts.database import Base, engine, session
+

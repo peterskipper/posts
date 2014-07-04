70b5a9413c6edce7e093a0965a45377c30fed0a6
Joe Turner <joe@oampo.co.uk>
2014-07-04 12:20:55 +0100

    Add test fixture.

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -10,3 +10,20 @@ from posts import app
 from posts import models
 from posts.database import Base, engine, session
 
+class TestAPI(unittest.TestCase):
+    """ Tests for the posts API """
+
+    def setUp(self):
+        """ Test setup """
+        self.client = app.test_client()
+
+        # Set up the tables in the database
+        Base.metadata.create_all(engine)
+
+    def tearDown(self):
+        """ Test teardown """
+        # Remove the tables and their data from the database
+        Base.metadata.drop_all(engine)
+
+if __name__ == "__main__":
+    unittest.main()
